1b8738be464926ac227636d86a45172d13b00ba5
Hannah Howard <hannah@hannahhoward.net>
2016-01-18 22:22:21 -0800

    Fix gitignore

--- frontend/build/src-es6/app/homepage/homepageControllers.js
+++
@@ -1,9 +0,0 @@
-import {Controller} from 'a1atscript';
-export function HomepageShowController() {}
-Object.defineProperty(HomepageShowController, "annotations", {get: function() {
-    return [new Controller('HomepageShowCtrl', [])];
-  }});
-export function HomepageController() {}
-Object.defineProperty(HomepageController, "annotations", {get: function() {
-    return [new Controller('HomepageCtrl', [])];
-  }});
