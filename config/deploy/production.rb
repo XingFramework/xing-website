57f73d00f675ce3272e07d6ef94143bfbe81beec
Hannah Howard <hannah@hannahhoward.net>
2016-01-19 15:41:30 -0800

    Deploy settings

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
-server 'example.com', user: 'root', roles: %w{web app}
+server 'appserver2.lrdesign.com', user: 'root', roles: %w{web app}
 set :branch, "production"
-set :application, "example.com"
+set :application, "xingframework.com"
 
-set :rails_warmup_url, 'http://example.com'
+set :rails_warmup_url, 'http://www.xingframework.com'
 
 # set :use_sudo, true # <- depends on deployment
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,6 @@
-server 'example.com', user: 'root', roles: %w{web app}
+server 'appserver2.lrdesign.com', user: 'root', roles: %w{web app}
 set :branch, "staging"
-set :application, "staging.example.com"
-set :rails_warmup_url, 'http://staging.example.com'
+set :application, "staging.xingframework.com"
+set :rails_warmup_url, 'http://staging.xingframework.com'
 
 #set :use_sudo, true #<- depends on deploy
